90ae2ac12260424b85e28e766dc2dffc4b4861a8
Chidimma <chidimmai@gmail.com>
2018-11-21 14:16:49 -0600

    Create node_linked_list.py

---
+++ node_linked_list.py
@@ -0,0 +1,5 @@
+class Node:
+    
+    def __init__(self, element):# node class constructor/creates an empty node
+        self._element = element# instance variable/ reference to each element
+        self._next = None #instance variable/ reference to next node
